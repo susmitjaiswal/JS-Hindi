3f65cfc3d1ecda6fdfa9fb7277044904c7e8cd49
susmitjaiswal <46866710+susmitjaiswal@users.noreply.github.com>
2024-03-14 16:36:52 +0000

    files saved

---
+++ node_basics/datatypes.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let account = null
+
+console.log(account)
--- node_basics/test.js
+++ node_basics/test.js
@@ -1,1 +1,5 @@
 console.log("Susmit")
+
+let name = "Susmit"
+const address = "85 Teacher's Colony"
+console.table([name,address])
